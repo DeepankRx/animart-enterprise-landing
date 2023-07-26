ef424b58bb773c24ef70ac102284b9fb29eef1ce
DeepankRx <deepankpushpad10@gmail.com>
2023-07-27 03:32:48 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,12 @@
 'use client';
 import React from 'react';
 import HomePage from '@/Pages/HomePage';
-import Aos from 'aos';
 import Head from 'next/head';
 const page = () => {
 
   return (
     <>
-      <Head>
-        <link
-          href="https://unpkg.com/aos@2.3.1/dist/aos.css"
-          rel="stylesheet"
-        />
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-      </Head>
+
       <HomePage />
     </>
   );
